703fdc92969c7039dd0de74657b6087b93713c25
Mithun Manivannan <mithunm93@gmail.com>
2016-04-24 21:19:18 -0700

    Don't ping slack on weekends
    This will run using UTC time, but since Casper only pings at 1PM PST,
    it will still be the same day in UTC. Should be fine, but may want
    to use moment to ensure PST day. Laziness prevails

--- slack.js
+++ slack.js
@@ -26,6 +26,10 @@ Slack.prototype.pingSlack = function(req, res) {
     return res.json(Slack.prototype.slackFormat(null, Errors.UNAUTHORIZED_ACCESS));
   }
 
+  // since this method is only called at 1PM PST, no need to account for time zone (UTC is 7 hours ahead, same day)
+  if (day = new Date().getDay() === 0 || day === 6) return console.log('Don\'t ping on weekend');
+  return console.log("ERROR");
+
   console.log('Taking orders now');
   Slack.prototype.send(null, 'Taking orders now');
   return res.status(200).end();
